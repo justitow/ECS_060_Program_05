f4e168d9ec2b60d31647ec40ebea9fca3bc4e581
justitow <justitow@gmail.com>
2016-05-29 04:26:57 -0700

    Worked on initializer

---
+++ StationMap.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Justin Weich on 5/29/16.
+//
+
+#include "StationMap.h"
---
+++ StationMap.h
@@ -0,0 +1,19 @@
+//
+// Created by Justin Weich on 5/29/16.
+//
+
+#ifndef ECS_060_PROGRAM_05_TRAINSTATIONS_H
+#define ECS_060_PROGRAM_05_TRAINSTATIONS_H
+
+
+#include "BinaryHeap.h"
+
+
+class StationMap
+{
+public:
+  StationMap();
+};
+
+
+#endif //ECS_060_PROGRAM_05_TRAINSTATIONS_H
